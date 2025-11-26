9555829d4cd391bfd158609c97b4dfa0f1e593f2
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2025-11-26 22:25:28 +0530

    Sidebar issue

--- FacultyM/src/Components/FacultyList.jsx
+++ FacultyM/src/Components/FacultyList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 // import { useNavigate } from "react-router-dom";
 import { FaTrash } from "react-icons/fa";
-import Sidebar from "../Components/Sidebar";
+
 
 const FacultyList = () => {
   const [facultyData, setFacultyData] = useState([]);
